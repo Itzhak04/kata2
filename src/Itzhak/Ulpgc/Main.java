d11f7108308cc28b85c9b47d21cd6342645e4b1a
itzhak <itzhakprieto@gmail.com>
2023-11-01 11:14:08 +0000

    implement main

--- src/Itzhak/Ulpgc/Main.java
+++ src/Itzhak/Ulpgc/Main.java
@@ -1,7 +1,23 @@
 package Itzhak.Ulpgc;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        LoaderChampion loader= new TsvFileLoaderChampion(new File("LoL.txt"));
+        List<Champion> champions= loader.load();
+        HashMap<String,Integer> chart= new   HashMap<>();
+        for (Champion champion: champions){
+            String clase= champion.getClase();
+            chart.put(clase,chart.getOrDefault(clase,0)+1);
+        }
+        for (String key: chart.keySet()){
+            System.out.println(key +": "+ chart.get(key));
+        }
+
 
     }
 }
